3f4921b23646ca7a72d7a86c5df655ed12b653e0
Connor Richardson <richardsonconnor09@gmail.com>
2024-04-29 21:06:51 +0100

    --updated formatting on multiple pages to be consistent with newer pages --Added JS code to improve user experience on mobile devices

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 window.addEventListener("load", myFunction);
+let count = 0
 function myFunction() {
     let menu = document.querySelector("#links");
+    const menuborder = document.querySelector("header");
     if (menu.style.display === "grid") {
         menu.style.display = "none";
+        menuborder.style.borderBottom = "1px solid black";
+        count++
     } else {
         menu.style.display = "grid";
+        if (count !== 0){
+            menuborder.style.borderBottom = "0";
+        }
+
     }
 }
